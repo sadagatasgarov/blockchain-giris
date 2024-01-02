76cf38c64810045da4b289fea63e43babdbd7135
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2024-01-02 23:45:26 +0400

    Protobuffer ile integrasiya etdik

--- types/block.go
+++ types/block.go
@@ -3,10 +3,15 @@ package types
 import (
 	"crypto/sha256"
 
+	"gitlab.com/sadagatasgarov/bchain/crypto"
 	"gitlab.com/sadagatasgarov/bchain/proto"
 
 	pb "google.golang.org/protobuf/proto"
 )
+
+func SignBlock(pk *crypto.PrivateKey, b *proto.Block) *crypto.Signature {
+	return pk.Sign(HashBlock(b))
+}
 
 // HashBlock returns a SHA256 of the header
 func HashBlock(block *proto.Block) []byte {
--- types/block_test.go
+++ types/block_test.go
@@ -4,8 +4,19 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gitlab.com/sadagatasgarov/bchain/crypto"
 	"gitlab.com/sadagatasgarov/bchain/util"
 )
+
+func TestSignBlock(t *testing.T) {
+	block:=util.RandomBlock()
+	privKey := crypto.GeneratePrivteKey()
+	pubKey := privKey.Public()
+
+	sig := SignBlock(privKey, block)
+	assert.Equal(t, 64, len(sig.Bytes()))
+	assert.True(t, sig.Verify(pubKey, HashBlock(block)))
+}
 
 func TestHashBlock(t *testing.T) {
 	block := util.RandomBlock()
